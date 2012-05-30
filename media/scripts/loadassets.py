df7ed55a4e7a07301d63d8447fec9ad54f509a30
dan rassi <dan@rassi.net>
2012-05-30 01:25:00 +0000

    add -ar 44100 -ac 2 to avconv params

--- media/scripts/loadassets.py
+++ media/scripts/loadassets.py
@@ -106,7 +106,7 @@ def queue_transcode_and_screenshot(asset_id):
     mp4_outpath = '%s.%s.%s' % (inpath, rand(4), mp4_derivative_type)
     mp4_outfile = os.path.join(Config.ASSET_ROOT, mp4_outpath)
     mp4_tmpfile = mp4_outfile + '.tmp'
-    mp4_cmd = "avconv -i %s -vcodec libx264 -vprofile high -preset slow -b:v 512k -maxrate 512k -bufsize 1024k -vf scale=-1:480 -acodec libvo_aacenc -b:a 128k -f mp4 -y %s" % (infile, mp4_tmpfile)
+    mp4_cmd = "avconv -i %s -vcodec libx264 -vprofile high -preset slow -b:v 512k -maxrate 512k -bufsize 1024k -vf scale=-1:480 -acodec libvo_aacenc -b:a 128k -ar 44100 -ac 2 -f mp4 -y %s" % (infile, mp4_tmpfile)
     faststart_cmd = 'qt-faststart %s %s' % (mp4_tmpfile, mp4_outfile)
     rm_tmp_cmd = 'rm %s' % mp4_tmpfile
     mp4_cmds = [mp4_cmd, faststart_cmd, rm_tmp_cmd]
